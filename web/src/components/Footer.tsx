c9b7a8d235c5ef839ee5b3bc1891150d937365a6
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2022-05-09 11:07:25 -0300

    feat: add footer component

---
+++ web/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer className="text-neutral-50 text-xs">
+      Feito por{' '}
+      <a
+        href="https://github.com/joseeduardotomazeli"
+        target="_blank"
+        className="underline"
+      >
+        José Eduardo Tomazeli
+      </a>{' '}
+      na NLW.
+    </footer>
+  );
+}
+
+export default Footer;
