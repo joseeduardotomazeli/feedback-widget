478b2ef3d4fd5679828ac94237448e653a1eee75
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2022-05-09 11:16:31 -0300

    feat: add content step component

---
+++ web/src/components/WidgetForm/Steps/ContentStep.tsx
@@ -0,0 +1,83 @@
+import { useState, FormEvent } from 'react';
+import { ArrowLeft } from 'phosphor-react';
+
+import { feedbackTypes, FeedbackType } from '..';
+
+import CloseButton from '../../CloseButton';
+import ScreenshotButton from '../ScreenshotButton';
+
+interface ContentStepProps {
+  feedbackType: FeedbackType;
+  onResetFeedback: () => void;
+  onSendFeedback: () => void;
+}
+
+function ContentStep(props: ContentStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [comment, setComment] = useState('');
+
+  const { feedbackType, onResetFeedback, onSendFeedback } = props;
+
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    console.log(screenshot);
+    console.log(comment);
+
+    onSendFeedback();
+  }
+
+  return (
+    <>
+      <header>
+        <button
+          type="button"
+          className="absolute top-5 left-5 text-zinc-400 hover:text-zinc-100"
+          onClick={onResetFeedback}
+        >
+          <ArrowLeft fontWeight="bold" className="w-4 h-4" />
+        </button>
+
+        <span className="flex items-center gap-2 text-xl leading-6">
+          <img
+            src={feedbackTypeInfo.image.source}
+            alt={feedbackTypeInfo.image.alt}
+            className="w-6 h-6"
+          />
+
+          {feedbackTypeInfo.title}
+        </span>
+
+        <CloseButton />
+      </header>
+
+      <form className="my-4 w-full" onSubmit={handleSubmit}>
+        <textarea
+          placeholder="Conte em detalhes o que está acontecendo..."
+          className="min-w-[304px] w-full min-h-[112px] border-zinc-600 focus:border-brand-500 rounded-md bg-transparent text-zinc-100 placeholder-zinc-400 text-sm resize-none scrollbar-thin scrollbar-track-transparent scrollbar-thumb-zinc-700"
+          value={comment}
+          onChange={(event) => setComment(event.target.value)}
+        />
+
+        <footer className="flex gap-2 mt-2">
+          <ScreenshotButton
+            screenshot={screenshot}
+            onScreenshotTaken={setScreenshot}
+          />
+
+          <button
+            type="submit"
+            className="flex flex-1 items-center justify-center p-2 border-transparent rounded-md bg-brand-500 hover:bg-brand-300 disabled:hover:bg-brand-500 text-sm disabled:opacity-50 transition-colors"
+            disabled={!comment.length}
+          >
+            Enviar feedback
+          </button>
+        </footer>
+      </form>
+    </>
+  );
+}
+
+export default ContentStep;
